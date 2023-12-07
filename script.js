d93a09610712ec4f897a59a1237ef6604fbdd849
AniDemi <AniDemi49@gmail.com>
2023-12-07 19:45:19 +0100

    V.2

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
 const choices = document.getElementById("choices");
 const shapeField = document.getElementById("testField");
+const testDesc = document.getElementById("testDesc");
+const startButton = document.getElementById("startButton");
+const inputField = document.getElementById("inputField");
+const submitResult = document.getElementById("submitResult");
 
-var shapes = 1;
+var trackShapes = 2;
+var intervals = [2000, 1000, 500, 250];
+var currentTest = 0;
+var squares;
+var circles;
+var triangles;
+var twoShapeResult = "";
+var oneShapeResult = "";
 
 
-
-function InitalizeTest(interval) {
+function startTest() {
+  console.log(intervals[currentTest]);
+  console.log(trackShapes);
   createTestScreen();
-  if (shapes == null) {
-    return console.log("You need to pick an amount of shapes first")
-  }
-  let squares = 0;
-  let circles = 0;
-  let triangles = 0;
-  let runsLeft = 20;
+  updateTestDesc();
+  
+  squares = 0;
+  circles = 0;
+  triangles = 0;
+  let runsLeft = 30;
   // Test time taken
-  // timetest = Date.now();
+  timetest = Date.now();
+  // Run test
   let testInterval = setInterval(function(){
     let shape = "";
     var result = Math.floor(Math.random() * 3)
@@ -39,13 +51,16 @@ function InitalizeTest(interval) {
     runsLeft--;
     if (runsLeft == 0) {
       clearInterval(testInterval);
-      shapeField.innerHTML = "";
-      createMenuScreen();
-      // Call to function to clear test, show result and input field
+      // Control variabels
+      setTimeout(() => {
+        shapeField.innerHTML = "";
+        inputResults();
+        
+      }, intervals[currentTest]);
     }
     // Test time taken
-    // console.log(Date.now() - timetest);
-  }, interval);
+    console.log(Date.now() - timetest);
+  }, intervals[currentTest]);
 }
 
 function create_shape(shape) {
@@ -58,11 +73,117 @@ function create_shape(shape) {
   shapeField.appendChild(form);
 }
 
-function createMenuScreen() {
-  choices.style.visibility = "visible";
+function createTestScreen() {
+  choices.style.display = "none";
 }
 
-function createTestScreen() {
-  choices.style.visibility = "hidden";
+function createMenuScreen(phase) {
+  submitResult.style.display = "none";
+  inputField.style.display = "none";
+  inputField.innerHTML = "";
+  choices.style.display = "block";
+  if (phase == "nowait") {
+    updateTestDesc();
+    startButton.innerHTML = "Continue";
+    choices.style.display = "block";
+  }
+  if (phase == "wait") {
+    updateTestDesc();
+    startButton.innerHTML = "Start";
+    choices.style.display = "block";
+  }
 }
+
+function inputResults() {
+  if (trackShapes == 2) {
+    createInput("circlecount", "Circles");
+    createInput("squarecount", "Squares");
+    //Two input fields
+    //Show results?
+  } else {
+    createInput("circlecount", "Circles");
+    //One input field
+    //Show results?
+  }
+  submitResult.style.display = "block";
+  inputField.style.display = "block";
+}
+
+function createInput(id, shape) {
+  const div = document.createElement("div");
+  const text = document.createElement("p");
+  const input = document.createElement("input");
+  input.setAttribute("id", id);
+  div.setAttribute("id", "inputStyle");
+
+  div.appendChild(text);
+  div.appendChild(input);
+  inputField.appendChild(div);
+  text.textContent = shape + ":";
+}
+
+function submitInput() {
+  if (trackShapes == 2) {
+    let squarecount = document.getElementById("squarecount").value;
+    let circlecount = document.getElementById("circlecount").value;
+    twoShapeResult += Math.round(10*100*(1-(Math.abs((circles-circlecount))/circles)))/10 + ", " + Math.round(10*100*(1-(Math.abs((squares-squarecount))/squares)))/10 + ", ";
+    console.log(twoShapeResult);
+  } else {
+    let circlecount = document.getElementById("circlecount").value;
+    oneShapeResult += Math.round(10*100*(1-(Math.abs((circles-circlecount))/circles)))/10 + ", ";
+    console.log(oneShapeResult);
+  }
+
+  currentTest++;
+  if (currentTest >= 4) {
+    currentTest = 0;
+    trackShapes--;
+    if (trackShapes == 0) {
+      createFinishScreen();
+      return
+    } else {
+      createMenuScreen("wait");
+      return
+    }
+  }
+  createMenuScreen("nowait");
+}
+
+function createFinishScreen() {
+  twoShapeResult = twoShapeResult.slice(0,-2);
+  twoShapeResult += ";";
+  oneShapeResult = oneShapeResult.slice(0,-2);
+  oneShapeResult += ";";
+  console.log(twoShapeResult);
+  console.log(oneShapeResult);
+  submitResult.style.display = "none";
+  shapeField.style.display = "none";
+  inputField.innerHTML = "";
+  testDesc.innerHTML = "You have finished all the tests " + "<br/>" + "please copy and paste the strings within the quotation marks found below into the google form"
+  createField(twoShapeResult, 2);
+  createField(oneShapeResult, 1);
+  //Create field below to copy from
+}
+
+function createField(content, type) {
+  text = document.createElement("p");
+  if (type == 2) {
+    text.textContent += "Results for counting two shapes: "
+  }
+  if (type == 1) {
+    text.textContent += "Results for counting one shape: "
+  }
+  text.textContent += "\""+content+"\"";
+  inputField.appendChild(text);
+}
+
+function updateTestDesc() {
+  if (trackShapes == 2) {
+    testDesc.innerHTML = "Each shape stays on the screen for " + intervals[currentTest] +"ms" + "<br/>" + "Your task is to count the amount of squares and circles shown";
+  } else {
+    testDesc.innerHTML = "Each shape stays on the screen for " + intervals[currentTest] +"ms" + "<br/>" + "Your task is to only count the amount of circles shown";
+  }
+}
+
+
 
